3b845d2ef9e190854341304d164f13d279a7a197
Alexander Tata <ta1996@yandex.ru>
2022-07-28 17:13:51 +0300

    Решение всех задач

--- Program.cs
+++ Program.cs
@@ -7,9 +7,11 @@
     int[] array = new int[size]; //добавляем переменную size для универсальности программы 
     FillArray(array, 100, 999);
     PrintArray(array);
-
+    KolChetnykhChisel(array);
+}
+void  KolChetnykhChisel(int[] array)
+{
     int count = 0;
-
     for (int i = 0; i < array.Length; i++)
     {
         if (array[i] % 2 == 0) count ++;
@@ -42,77 +44,51 @@ void PrintArray(int[] array)
 
 }  
 
-
-void Zadacha27()
-{   
-    //Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-
-    Random random = new Random();
-    int number = random.Next(333, 777);
-
-    SumOfDigit(number);
-
-}
-
-void SumOfDigit(int number)
+void Zadacha36()
 {
+    //Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+    //Найдите сумму элементов, стоящих на нечётных позициях.
+    // в данной задачи, считаем по нечетным индексам 
+    int size = 8;
+    int[] array = new int[size]; 
+    FillArray(array, 0, 10);
+    PrintArray(array);
+
     int sum = 0;
 
-    Console.Write("В числе " + number);
-
-    while(number > 0)
+    for (int i = 0; i < array.Length; i++)
     {
-        sum += number % 10;
-        number /= 10;
+        if (i % 2 == 1) sum += array[i];
     }
 
-    Console.WriteLine(" сумма цифр равна: " + sum);
+    Console.WriteLine("сумма элементов стоящих на нечётных позициях равна: " + sum  );
 
 }
 
-void Zadacha29()
+void Zadacha38()
 {
-    //Задача 29: Напишите программу, которая задаёт массив из 8 случайных целых чисел и выводит 
-    //отсортированный по модулю массив
+    //Задача 38: Задайте массив вещественных чисел. 
+    //Найдите разницу между максимальным и минимальным элементов массива.
 
-    int size = 8;
-    int[] array = new int[size];
     Random random = new Random();
+    double[] array = new double[10];
 
     for (int i = 0; i < array.Length; i++)
     {
-        array[i] = random.Next(-10, 11);
+        array[1] = random.NextDouble();
+    }
+
+    Console.WriteLine("Вывод массива ");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t" );
     }
 
     Console.WriteLine();
-    Console.WriteLine("Вывод массива: ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + "\t");
-    }
-    Console.WriteLine();
 
-    for (int i = array.Length - 1; i > 0; i--)
-    {
-        for (int j = 0; j < i; j++)
-        {
-            if (Math.Abs(array[j]) > Math.Abs(array[j + 1]))
-            {
-                int temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-            }
-        }
-    }
-    Console.WriteLine();
-    Console.WriteLine("Вывод массива: ");
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + "\t");
-    }
-    Console.WriteLine();
+
 }
-
-Zadacha34();
-//Zadacha27();
-//Zadacha29();
+//Zadacha34();
+//Zadacha36();
+Zadacha38();
